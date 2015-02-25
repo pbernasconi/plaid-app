c45c4dcbbffd099aa51a62887367edffbe72df91
Paolo Bernasconi <paolo.enrico.bernasconi@gmail.com>
2015-02-25 18:55:37 -0500

    editorconfig + formatting

--- www/js/controllers.js
+++ www/js/controllers.js
@@ -1,141 +1,137 @@
 angular.module('starter.controllers', [])
 
-		.controller('AppCtrl', function ($scope, $ionicModal, $timeout) {
-				// Form data for the login modal
-				$scope.loginData = {};
+  .controller('AppCtrl', function ($scope, $ionicModal, $timeout) {
+    // Form data for the login modal
+    $scope.loginData = {};
 
-				// Create the login modal that we will use later
-				$ionicModal.fromTemplateUrl('templates/login.html', {
-						scope: $scope
-				}).then(function (modal) {
-						$scope.modal = modal;
-				});
+    // Create the login modal that we will use later
+    $ionicModal.fromTemplateUrl('templates/login.html', {
+      scope: $scope
+    }).then(function (modal) {
+      $scope.modal = modal;
+    });
 
-				// Triggered in the login modal to close it
-				$scope.closeLogin = function () {
-						$scope.modal.hide();
-				};
+    // Triggered in the login modal to close it
+    $scope.closeLogin = function () {
+      $scope.modal.hide();
+    };
 
-				// Open the login modal
-				$scope.login = function () {
-						$scope.modal.show();
-				};
+    // Open the login modal
+    $scope.login = function () {
+      $scope.modal.show();
+    };
 
-				// Perform the login action when the user submits the login form
-				$scope.doLogin = function () {
-						console.log('Doing login', $scope.loginData);
+    // Perform the login action when the user submits the login form
+    $scope.doLogin = function () {
+      console.log('Doing login', $scope.loginData);
 
-						// Simulate a login delay. Remove this and replace with your login
-						// code if using a login system
-						$timeout(function () {
-								$scope.closeLogin();
-						}, 1000);
-				};
-		})
-
-
-		.controller('InstitutionsCtrl', function ($scope, $stateParams, $ionicScrollDelegate, Plaid) {
+      // Simulate a login delay. Remove this and replace with your login
+      // code if using a login system
+      $timeout(function () {
+        $scope.closeLogin();
+      }, 1000);
+    };
+  })
 
 
+  .controller('InstitutionsCtrl', function ($scope, $stateParams, $ionicScrollDelegate, Plaid) {
 
 
+    $scope.searchInput = "";
+    var searchInputEle = angular.element(document.getElementsByClassName('search-input'));
+
+    $scope.scrollTop = function () {
+      $ionicScrollDelegate.scrollTop(true);
+      searchInputEle[0].focus();
+    };
+
+    $scope.clearInput = function () {
+      $scope.searchInput = "";
+    };
+
+    Plaid.getInstitutions().then(function (result) {
+      $scope.institutions = result.data || noop;
+    }, function (error) {
+      console.error(error);
+    });
+
+  })
 
 
-				$scope.searchInput = "";
-				var searchInputEle = angular.element(document.getElementsByClassName('search-input'));
+  .controller('CategoriesCtrl', function ($scope, $rootScope, $stateParams, $timeout, $ionicScrollDelegate, Plaid) {
 
-				$scope.scrollTop = function () {
-						$ionicScrollDelegate.scrollTop(true);
-						searchInputEle[0].focus();
-				};
+    $scope.searchInput = "";
+    $scope.slideHeader = false;
+    $scope.slideHeaderPrevious = 0;
 
-				$scope.clearInput = function () {
-						$scope.searchInput = "";
-				};
+    var searchInputEle = angular.element(document.getElementsByClassName('search-input'));
 
-				Plaid.getInstitutions().then(function (result) {
-						$scope.institutions = result.data || noop;
-				}, function (error) {
-						console.error(error);
-				});
+    $scope.scrollTop = function () {
+      $ionicScrollDelegate.scrollTop(true);
+      searchInputEle[0].focus();
+    };
 
-		})
+    $scope.clearInput = function () {
+      $scope.searchInput = "";
+    };
 
 
-		.controller('CategoriesCtrl', function ($scope, $rootScope, $stateParams, $timeout, $ionicScrollDelegate, Plaid) {
+    // get All institutions
+    Plaid.getCategories().then(function (result) {
+      $scope.categories = result.data;
+    }, function (error) {
+      console.error(error);
+    })
 
-				$scope.searchInput = "";
-				$scope.slideHeader = false;
-				$scope.slideHeaderPrevious = 0;
-
-				var searchInputEle = angular.element(document.getElementsByClassName('search-input'));
-
-				$scope.scrollTop = function () {
-						$ionicScrollDelegate.scrollTop(true);
-						searchInputEle[0].focus();
-				};
-
-				$scope.clearInput = function () {
-						$scope.searchInput = "";
-				};
+  })
 
 
-				// get All institutions
-				Plaid.getCategories().then(function (result) {
-						$scope.categories = result.data;
-				}, function (error) {
-						console.error(error);
-				})
+  .controller('ConnectCtrl', function ($scope, $state, $stateParams, Plaid, ConnectStore) {
 
-		})
-
-
-		.controller('ConnectCtrl', function ($scope, $state, $stateParams, Plaid, ConnectStore) {
-
-				$scope.user = {
-						type: '',
-						name: 'Plaid Test',
-						username: 'plaid_test',
-						email: 'pl@i.d',
-						password: 'plaid_good'
-				};
+    $scope.user = {
+      type: '',
+      name: 'Plaid Test',
+      username: 'plaid_test',
+      email: 'pl@i.d',
+      password: 'plaid_good'
+    };
 
 
-				// get All institutions
-				Plaid.getInstitutions().then(function (result) {
-						$scope.institutions = result.data;
-						//console.log(result.data);
-				}, function (error) {
-						console.error(error);
-				});
+    // get All institutions
+    Plaid.getInstitutions().then(function (result) {
+      $scope.institutions = result.data;
+      //console.log(result.data);
+    }, function (error) {
+      console.error(error);
+    });
 
-				$scope.connect = function (user) {
+    $scope.connect = function (user) {
 
-						Plaid.connect(user.type, user.username, user.password).then(function (result) {
-								console.log(result);
+      Plaid.connect(user.type, user.username, user.password).then(function (result) {
+        console.log(result);
 
-								if (result.status == 200) {
-										ConnectStore.save(result.data);
-										$state.go('app.connect-detail');
-								}
+        if (result.status == 200) {
+          ConnectStore.save(result.data);
+          $state.go('app.connect-detail');
+        }
 
-								if (result.status == 201) {
-										alert("MFA required");
-								}
+        if (result.status == 201) {
+          alert("MFA required");
+        }
 
-						}, function (error) {
-								alert("There was an error connecting\n ERROR: " + error.data.message + '\n RESOLVE: ' + error.data.resolve);
-						});
-				};
+      }, function (error) {
+        alert("There was an error connecting\n ERROR: " + error.data.message + '\n RESOLVE: ' + error.data.resolve);
+      });
+    };
 
-		})
-
-
-		.controller('ConnectDetailCtrl', function ($scope, $state, $stateParams, Plaid, ConnectStore) {
+  })
 
 
-				$scope.connection = ConnectStore.get();
+  .controller('ConnectDetailCtrl', function ($scope, $state, $stateParams, Plaid, ConnectStore) {
 
-				console.log($scope.connection);
 
-		});
+    $scope.connection = ConnectStore.get();
+
+    console.log($scope.connection);
+
+  });
--- www/js/plaid.service.js
+++ www/js/plaid.service.js
@@ -1,99 +1,99 @@
 angular.module('starter.plaid.service', [])
 
-		.value('API_URL', "https://tartan.plaid.com")
-		.value('plaid_client_id', "test_id")
-		.value('plaid_secret', "test_secret")
+  .value('API_URL', "https://tartan.plaid.com")
+  .value('plaid_client_id', "test_id")
+  .value('plaid_secret', "test_secret")
 
-		.factory('Plaid', function ($http, API_URL, plaid_client_id, plaid_secret) {
+  .factory('Plaid', function ($http, API_URL, plaid_client_id, plaid_secret) {
 
-				return {
+    return {
 
-						getInstitutions: function () {
-								return $http.get(API_URL + "/institutions");
-						},
+      getInstitutions: function () {
+        return $http.get(API_URL + "/institutions");
+      },
 
-						getCategories: function () {
-								return $http.get(API_URL + "/categories");
-						},
+      getCategories: function () {
+        return $http.get(API_URL + "/categories");
+      },
 
-						connect: function (type, username, password, pin, options) {
-								return $http.post(API_URL + "/connect", {
-										client_id: plaid_client_id,
-										secret: plaid_secret,
-										type: type,
-										username: username,
-										password: password,
-										pin: pin || null,
-										options: options || null
-								});
-						},
+      connect: function (type, username, password, pin, options) {
+        return $http.post(API_URL + "/connect", {
+          client_id: plaid_client_id,
+          secret: plaid_secret,
+          type: type,
+          username: username,
+          password: password,
+          pin: pin || null,
+          options: options || null
+        });
+      },
 
-						connectStep: function (type, username, password, pin, options) {
-								return $http.post(API_URL + "/connect/step", {
-										client_id: plaid_client_id,
-										secret: plaid_secret,
-										type: type,
-										username: username,
-										password: password,
-										pin: pin || null,
-										options: options || null
-								});
-						},
+      connectStep: function (type, username, password, pin, options) {
+        return $http.post(API_URL + "/connect/step", {
+          client_id: plaid_client_id,
+          secret: plaid_secret,
+          type: type,
+          username: username,
+          password: password,
+          pin: pin || null,
+          options: options || null
+        });
+      },
 
-						connectGet: function (type, username, password, pin, options) {
-								return $http.post(API_URL + "/connect/get", {
-										client_id: plaid_client_id,
-										secret: plaid_secret,
-										type: type,
-										username: username,
-										password: password,
-										pin: pin || null,
-										options: options || null
-								});
-						},
-
-
-						balance: function (access_token) {
-								return $http.post(API_URL + "/balance", {
-										client_id: plaid_client_id,
-										secret: plaid_secret,
-										access_token: access_token
-								});
-						},
+      connectGet: function (type, username, password, pin, options) {
+        return $http.post(API_URL + "/connect/get", {
+          client_id: plaid_client_id,
+          secret: plaid_secret,
+          type: type,
+          username: username,
+          password: password,
+          pin: pin || null,
+          options: options || null
+        });
+      },
 
 
-						upgrade: function (access_token, upgrade_to, options) {
-								return $http.post(API_URL + "/upgrade", {
-										client_id: plaid_client_id,
-										secret: plaid_secret,
-										access_token: access_token,
-										upgrade_to: upgrade_to,
-										options: options
-								});
-						},
+      balance: function (access_token) {
+        return $http.post(API_URL + "/balance", {
+          client_id: plaid_client_id,
+          secret: plaid_secret,
+          access_token: access_token
+        });
+      },
 
 
-						auth: function () {
-								return $http.post(API_URL + "/auth", {
-										client_id: plaid_client_id,
-										secret: plaid_secret
-								});
-						}
-				}
-		})
+      upgrade: function (access_token, upgrade_to, options) {
+        return $http.post(API_URL + "/upgrade", {
+          client_id: plaid_client_id,
+          secret: plaid_secret,
+          access_token: access_token,
+          upgrade_to: upgrade_to,
+          options: options
+        });
+      },
 
 
-		.factory('ConnectStore', function () {
+      auth: function () {
+        return $http.post(API_URL + "/auth", {
+          client_id: plaid_client_id,
+          secret: plaid_secret
+        });
+      }
+    }
+  })
 
-				var connectionStore = this.connectionStore = {};
 
-				return {
-						save: function (connection) {
-								connectionStore = connection;
-						},
+  .factory('ConnectStore', function () {
 
-						get: function () {
-								return connectionStore;
-						}
-				}
-		});
+    var connectionStore = this.connectionStore = {};
+
+    return {
+      save: function (connection) {
+        connectionStore = connection;
+      },
+
+      get: function () {
+        return connectionStore;
+      }
+    }
+  });
